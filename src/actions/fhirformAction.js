0246475674211dc5b75dd88ecd13776422e8a012
Bell Eapen <github@gulfdoctor.net>
2018-08-17 14:16:37 -0400

    access store added.

--- src/actions/fhirformAction.js
+++ src/actions/fhirformAction.js
@@ -1,7 +1,7 @@
-import { Actions } from "@jsonforms/core";
+﻿import { Actions } from "@jsonforms/core";
 import { LOAD_FORM } from "../constants/ActionTypes";
 import questionnaireService from "../services/questionnaireService";
-
+import configureStore from "../store/index"
 
 	export function loadForm(_base, _uri, _id, _version) {
 	  return {type: LOAD_FORM, payload: questionnaireService.getQuestionnaire(_base, _uri, _id, _version)};
@@ -20,6 +20,8 @@ import questionnaireService from "../services/questionnaireService";
 		}
 	}
 
-	export function handleSubmitAction(_questionnaireResponse) {
+    export function handleSubmitAction(_questionnaireResponse) {
+        let state = configureStore.getState();
+        let core_data = state.jsonforms.core;
 	  	return true;
 	}
