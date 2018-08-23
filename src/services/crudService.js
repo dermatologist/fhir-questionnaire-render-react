7b302ec94e5f52170b49f5dc31fe4d98bb45f43d
dermatologist <github@gulfdoctor.net>
2018-08-22 20:17:03 -0400

    post updated to form data

--- src/services/crudService.js
+++ src/services/crudService.js
@@ -1,3 +1,4 @@
+import axios from "axios";
 import { fhirServer } from "./crud";
 
 
@@ -11,19 +12,43 @@ export default class crudService {
   }
 
   static postResource(url, qResponse) {
-    // url = 'http://hapi.fhir.org/create'; // TODO: Change this URL
-    url = "create"; // TODO: Change this URL
-
-    // url = 'http://localhost:3000/';
+    // // url = 'http://hapi.fhir.org/create'; // TODO: Change this URL
+    // url = "create"; // TODO: Change this URL
+    //
+    // // url = 'http://localhost:3000/';
     qResponse.status = "completed";
     delete qResponse.id; // Delete any existing ID, will be aded by the server
+    //
+    // const toPost = {};
+    // toPost.serverId = "home";
+    // toPost.resource = "QuestionnaireResponse";
+    // toPost["resource-create-id"] = "";
+    // toPost["resource-create-body"] = qResponse;
+    // return fhirServer.post(url, toPost);
 
-    const toPost = {};
-    toPost.serverId = "home";
-    toPost.resource = "QuestionnaireResponse";
-    toPost["resource-create-id"] = "";
-    toPost["resource-create-body"] = qResponse;
-    return fhirServer.post(url, toPost);
+    let toReturn = null;
+    const bodyFormData = new FormData();
+    bodyFormData.set('serverId', 'home');
+    bodyFormData.set('resource', 'QuestionnaireResponse');
+    bodyFormData.set('resource-create-id', '');
+    bodyFormData.set('resource-create-body', qResponse);
+
+    axios({
+      method: 'post',
+      url: 'create',
+      data: bodyFormData,
+      config: { headers: {'Content-Type': 'multipart/form-data' }}
+    })
+      .then(response => {
+        // handle success
+        console.log(response);
+        toReturn = response;
+      })
+      .catch(response => {
+        // handle error
+        console.log(response);
+      });
+    return toReturn;
   }
 
   static getResource(baseUrl, uri, _id, _version) {
