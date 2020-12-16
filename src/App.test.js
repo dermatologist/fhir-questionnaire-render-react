3bf40562d420e13e2463a3502a30c3e69843793f
Bell Eapen <github@gulfdoctor.net>
2020-12-15 20:15:21 -0500

    working tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,12 @@
-import React from 'react';
-import { render } from '@testing-library/react';
 import App from './App';
+import { shallow } from 'enzyme';
 
-test('renders FHIR Questionnaire edit header', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/FHIR Questionnaire/i);
-  expect(linkElement).toBeInTheDocument();
+it("renders without crashing", () => {
+  shallow(<App />);
+});
+
+it("renders Account header", () => {
+  const wrapper = shallow(<App />);
+  const welcome = <h2>Edit FHIR Questionnaire below (Change IDs if you duplicate an element)</h2>
+  expect(wrapper.contains(welcome)).toEqual(true);
 });
